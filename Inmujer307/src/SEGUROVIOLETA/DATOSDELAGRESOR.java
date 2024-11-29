f3261d3a1c329ab97798c88881c1727075fc668c
ilse_ <ilse_@LAPTOP-3HDI7LAR>
2024-11-29 12:13:36 -0600

    Cambio tipografia

--- Inmujer307/src/SEGUROVIOLETA/DATOSDELAGRESOR.java
+++ Inmujer307/src/SEGUROVIOLETA/DATOSDELAGRESOR.java
@@ -120,7 +120,7 @@ public class DATOSDELAGRESOR extends JFrame {
 	 */
 	public DATOSDELAGRESOR() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 823, 287);
+		setBounds(100, 100, 823, 650);
 		contentPane = new JPanel();
 		contentPane.setBackground(new Color(247, 231, 245));
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
